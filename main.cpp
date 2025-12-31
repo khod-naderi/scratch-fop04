d6104dd7c814835a13051e75d4b66eef5d2d5139
khod-naderi <khod.naderi@gmail.com>
2025-12-31 22:31:53 +0330

    Add some comments to main.cpp

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 
 int main(int argc, char *argv[])
 {
+    // Initialize SDL2 everything
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
     {
         std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return SDL_INITIALIZE_ERROR;
     }
 
+    // Creating a static windows
     SDL_Window *window = SDL_CreateWindow(
         "Scratch",
         SDL_WINDOWPOS_CENTERED,
@@ -20,24 +22,27 @@ int main(int argc, char *argv[])
 
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
-    bool running = true;
-    SDL_Event event;
+    bool running = true; // this is root app running indenticator
+
+    SDL_Event eventSDL;
 
     while (running)
     {
-        while (SDL_PollEvent(&event))
+        while (SDL_PollEvent(&eventSDL))
         {
-            if (event.type == SDL_QUIT)
+            // handle Quit button on top-right corner of page
+            if (eventSDL.type == SDL_QUIT)
             {
                 running = false;
             }
         }
 
-        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // Set background color to white.
         SDL_RenderClear(renderer);
         SDL_RenderPresent(renderer);
     }
 
+    // Exiting from program.
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
