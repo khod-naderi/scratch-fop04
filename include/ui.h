3699fbd6f7b94d0f5ec509c518e3e0190430525a
khod-naderi <khod.naderi@gmail.com>
2026-01-01 18:36:52 +0330

    use extern to prevent from linker error.

--- include/ui.h
+++ include/ui.h
@@ -17,12 +17,12 @@ This header file is for handling UI component
 #define CANVAS_SCREEN_WIDTH 500
 #define CANVAS_SCREEN_HEIGTH 500
 
-const SDL_Rect MENUBAR_ROW = {0, 0, MAIN_WINDOW_WIDTH, MENUBAR_HEIGHT};
-const SDL_Rect CATEGORY_COLUMN = {0, MENUBAR_HEIGHT, CATEGORY_COLUMN_WIDTH, MAIN_WINDOW_HEIGHT};
-const SDL_Rect BLOCKS_COLUMN = {CATEGORY_COLUMN_WIDTH, MENUBAR_HEIGHT, CATEGORY_COLUMN_WIDTH + BLOCKS_COLUMN_WIDTH, MAIN_WINDOW_HEIGHT};
-const SDL_Rect WORKSPACE_COLUMN = {CATEGORY_COLUMN_WIDTH + BLOCKS_COLUMN_WIDTH, MENUBAR_HEIGHT, MAIN_WINDOW_WIDTH - CANVAS_SCREEN_WIDTH, MAIN_WINDOW_HEIGHT};
-const SDL_Rect CANVAS_BOX = {MAIN_WINDOW_WIDTH - CANVAS_SCREEN_WIDTH, MENUBAR_HEIGHT, MAIN_WINDOW_WIDTH, CANVAS_SCREEN_HEIGTH + MENUBAR_HEIGHT};
-const SDL_Rect SPRITE_BOX = {MAIN_WINDOW_WIDTH - CANVAS_SCREEN_WIDTH, CANVAS_SCREEN_HEIGTH + MENUBAR_HEIGHT, MAIN_WINDOW_WIDTH, MAIN_WINDOW_HEIGHT};
+extern const SDL_Rect MENUBAR_ROW;
+extern const SDL_Rect CATEGORY_COLUMN;
+extern const SDL_Rect BLOCKS_COLUMN;
+extern const SDL_Rect WORKSPACE_COLUMN;
+extern const SDL_Rect CANVAS_BOX;
+extern const SDL_Rect SPRITE_BOX;
 
 SDL_Texture *renderText(SDL_Renderer *renderer, TTF_Font *font, const char *text, SDL_Color color);
 bool isPointInRect(int x, int y, SDL_Rect rect);
@@ -44,7 +44,7 @@ extern SDL_Texture *fileMenuText;
 extern SDL_Texture *editMenuText;
 
 // Define menu reactangles
-const SDL_Rect fileMenuRect = {LOGO_MARGIN_LEFT + LOGO_WIDTH + 2 * MENU_MARGIN_LEFT, 5, 60, MENUBAR_HEIGHT - 10};
-const SDL_Rect editMenuRect = {fileMenuRect.x + fileMenuRect.w + MENU_MARGIN_LEFT, 5, 60, MENUBAR_HEIGHT - 10};
+extern const SDL_Rect fileMenuRect;
+extern const SDL_Rect editMenuRect;
 
 #endif
