6753ec69880ef59d3770ac08141d76f75e5f6b29
khod-naderi <khod.naderi@gmail.com>
2026-02-11 16:05:11 +0330

    better blocks manging struct for handling nesting structures and diffrent type blocks

--- include/block.h
+++ include/block.h
@@ -4,6 +4,7 @@
 #include <SDL2/SDL_ttf.h>
 #include "category.h"
 #include <vector>
+#include "value.h"
 
 /*
 This header file is for handling command Blocks
@@ -14,33 +15,79 @@ And Ui part of Blocks list
 #define BLOCK_HEADER
 
 #define BLOCK_ITEM_HEIGTH 50
+#define BLOCK_MAX_SLOTS 8
+#define BLOCK_MAX_BODIES 2
 
-enum block_t
+/*
+Blocks type
+*/
+enum BlockType
 {
     BLOCK_COMMAND,
     BLOCK_EVENT,
     BLOCK_STATEMENT,
     BLOCK_OPERATOR,
+    BLOCK_REPORTER,
 };
+
+/*
+For handling slots of blocks
+*/
+struct SlotDefinition
+{
+    const char *name;
+    ValueType dateType;
+    Value defaultValue;
+};
+
+/*
+For handling execution context for each block
+witch sprint, witch position, etc.
+*/
+struct ExecutionContext;
+
+/*
+this is type for execution function of each block,
+what that block should do .
+it is a function pointer
+*/
+typedef Value (*BlockExecuteFunc)(ExecutionContext &ctx, Value inputs[], int inputCount);
 
 struct Block
 {
-    const char *label;
-    const CATEGORY_ID categoryId;
-    const block_t type;
-    void (*command)();
+    int id;            // identifier of block defenition
+    const char *label; // label of block defenition
+    CategoryId categoryId;
+    BlockType type;
+
+    // input slots
+    SlotDefinition slots[BLOCK_MAX_SLOTS];
+    int slotCount;
+
+    // Output type for operators and reporters
+    ValueType outputType;
+
+    // Old method
+    // // void (*command)();
     int width, height;
     SDL_Texture *textrue;
 
-    // for connection in workspace
+    // shape and connecting
     const bool canHaveTopConnection;
     const bool canHaveBottomConnection;
+    int bodyCount;
 
-    Block(const char *f_label, const CATEGORY_ID f_categoryId, const block_t f_type, const bool f_canHaveTopConnection, const bool f_canHaveBottomConnection, void (*command)())
-        : label(f_label), categoryId(f_categoryId), type(f_type), canHaveTopConnection(f_canHaveTopConnection), canHaveBottomConnection(f_canHaveBottomConnection), command(command)
+    // execution
+    BlockExecuteFunc execute;
+
+    // base rendering dimension
+    int baseWidth, baseHeight;
+
+    Block(const char *f_label, const CategoryId f_categoryId, const BlockType f_type, const bool f_canHaveTopConnection, const bool f_canHaveBottomConnection, BlockExecuteFunc command)
+        : label(f_label), categoryId(f_categoryId), type(f_type), canHaveTopConnection(f_canHaveTopConnection), canHaveBottomConnection(f_canHaveBottomConnection), execute(command)
     {
     }
-    Block(const char *f_label, const CATEGORY_ID f_categoryId, const block_t f_type, const bool f_canHaveTopConnection, const bool f_canHaveBottomConnection)
+    Block(const char *f_label, const CategoryId f_categoryId, const BlockType f_type, const bool f_canHaveTopConnection, const bool f_canHaveBottomConnection)
         : label(f_label), categoryId(f_categoryId), type(f_type), canHaveTopConnection(f_canHaveTopConnection), canHaveBottomConnection(f_canHaveBottomConnection)
     {
     }
