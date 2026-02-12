521310fa90e4b343f55b5ed018c2e816f0656eef
khod-naderi <khod.naderi@gmail.com>
2026-02-12 15:29:21 +0330

    FIX: code space header declear

--- include/codespace.h
+++ include/codespace.h
@@ -13,3 +13,12 @@ struct CodeSpace
 };
 
 void codespaceInit(CodeSpace &cs);
+
+int codespaceCreateInstance(CodeSpace &cs, int definitionId, int posX, int posY);
+BlockInstance *codespaceGetInstance(CodeSpace &cs, int id);
+void codespaceAttachNext(CodeSpace &cs, int topId, int bottomId);
+void codespaceAttachToBody(CodeSpace &cs, int parentId, int bodyIndex, int childId);
+void codespaceAttachToInput(CodeSpace &cs, int hostId, int slotIndex, int operatorId);
+void codespaceDetach(CodeSpace &cs, int id);
+void codespaceRemoveInstance(CodeSpace &cs, int id);
+int codespaceFindEventBlocks(CodeSpace &cs, int *outIds, int maxOut);
