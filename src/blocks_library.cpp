cfc2d7cc7ceb483e61765ed77a6ee1279abc4e5e
khod-naderi <khod.naderi@gmail.com>
2026-02-12 16:22:19 +0330

    Add more blocks to block library

--- src/blocks_library.cpp
+++ src/blocks_library.cpp
@@ -31,6 +31,17 @@ Value execGreenflag(ExecutionContext &ctx, Value inputs[], int inputCount)
     return Value();
 }
 
+Value execRepeat(ExecutionContext &ctx, Value inputs[], int inputCount)
+{
+    // I do not know how to handle this !!
+    return inputs[0];
+}
+
+Value execAdd(ExecutionContext &ctx, Value inputs[], int inputCount)
+{
+    return fromNumber(inputs[0].asNumber() + inputs[1].asNumber());
+}
+
 Block blocksLibrary[] = {
     // [0] move %s steps
     {
@@ -66,6 +77,62 @@ Block blocksLibrary[] = {
         execGreenflag,
         200,
         40,
+    },
+    // [2] move %s steps
+    {
+        2,                // id
+        "turn %s degree", // label
+        CATEGORY_MOTION,  // category id
+        BLOCK_COMMAND,    // type
+        {
+            // slots
+            {"angle", VALUE_NUMBER, fromNumber(15)},
+        },
+        1,                   // slotCount
+        VALUE_NUMBER,        // output tyep
+        true,                // canHaveTopConnection
+        true,                // canHaveBottomConnection
+        0,                   // body count
+        execTurnAngleDegree, // execution
+        150,                 // base width
+        40,                  // base height
+    },
+    // [3] Repeat %s
+    {
+        3,
+        "repeat %s",
+        CATEGORY_CONTROL,
+        BLOCK_STATEMENT,
+        {
+            {"count", VALUE_NUMBER, fromNumber(10)},
+        },
+        1,
+        VALUE_NUMBER,
+        true,
+        true,
+        1,
+        execRepeat,
+        150,
+        40,
+    },
+    // [4] %s + %s
+    {
+        4,
+        "%s + %s",
+        CATEGORY_OPERATOR,
+        BLOCK_OPERATOR,
+        {
+            {"a", VALUE_NUMBER, fromNumber(0)},
+            {"b", VALUE_NUMBER, fromNumber(0)},
+        },
+        2,
+        VALUE_NUMBER,
+        false,
+        false,
+        0,
+        execAdd,
+        100,
+        30,
     },
 };
 
