d6679105b9dd89805b0688d9b580151d11fd121c
Amirreza Liravi <liraviamirreza85@gmail.com>
2026-02-12 11:41:14 +0330

    thumbnail display with hover effect, hover effect is not complete yet.

--- src/spritebox.cpp
+++ src/spritebox.cpp
@@ -63,6 +63,7 @@ This function is for drawing Sprite box.
 this function will run on every frame.
 -------------------------------------------------
 */
+
 void drawSpriteBoxScreen(SDL_Renderer *renderer, TTF_Font *font, const int mouseX, const int mouseY)
 {
     // Draw backgound color
@@ -81,10 +82,67 @@ void drawSpriteBoxScreen(SDL_Renderer *renderer, TTF_Font *font, const int mouse
 
     // Sprite Area
     SDL_Rect spriteArea = { SPRITE_BOX.x, tobBar.y + tobBar.h, SPRITE_BOX.w, SPRITE_BOX.h - tobBar.h };
-    SDL_SetRenderDrawColor(renderer, color_white);
+    SDL_SetRenderDrawColor(renderer, 200, 220, 255, 255);
     SDL_RenderFillRect(renderer, &spriteArea);
     SDL_SetRenderDrawColor(renderer, color_black);
     SDL_RenderDrawRect(renderer, &spriteArea);
+
+    // Sprtie thumbnails
+    const int thumbnailSize = 70;
+    const int spacing = 10;
+    int StartX = spriteArea.x + spacing; 
+    int StartY = spriteArea.y + spacing;
+
+    for ( int i = 0; i < aliveSprints.size(); i++ )
+    {
+        SprintBody& sprite = aliveSprints[i];
+
+        // position of thumbnail
+        int thumbnailX = StartX + i * (thumbnailSize + spacing);
+        int thumbnailY = StartY;
+
+        // Draw thumbnail
+        SDL_Rect thumbRect = {thumbnailX , thumbnailY, thumbnailSize, thumbnailSize};
+        SDL_RenderCopy (renderer, sprite.nowCustome, nullptr, &thumbRect);
+
+        // Draw border
+        SDL_SetRenderDrawColor(renderer, color_black);
+        SDL_RenderDrawRect(renderer, &thumbRect);
+    }
+
+
+    // Hover effect on thumbnails
+    for ( int i = 0; i < aliveSprints.size(); i++ ){
+        SprintBody &sprite = aliveSprints[i];
+
+        int thumbnailX = StartX + i * ( thumbnailSize + spacing ); 
+        int thumbnailY = StartY;
+
+        SDL_Rect thumbRect = { thumbnailX, thumbnailY, thumbnailSize, thumbnailSize };
+        
+        // check if mouse is hovering over the thumbnail
+        bool isHover = ( mouseX >= thumbRect.x && mouseX <= ( thumbRect.x + thumbRect.w ) &&
+                         mouseY >= thumbRect.y && mouseY <= ( thumbRect.y + thumbRect.h ) );
+        if ( isHover) {
+            SDL_SetRenderDrawColor(renderer, 200, 220, 255, 255); // light blue for hover effect
+        }
+        else {
+            SDL_SetRenderDrawColor(renderer, 120, 120, 120, 255);
+        }
+        SDL_RenderFillRect(renderer, &thumbRect);
+
+        SDL_RenderCopy(renderer, sprite.nowCustome, nullptr, &thumbRect);
+
+        // Draw border
+        if ( isHover ) {
+            SDL_SetRenderDrawColor(renderer, 100, 100, 255, 255); // blue border for hover effect
+        }
+        else {
+            SDL_SetRenderDrawColor(renderer, color_black);
+        }
+
+        SDL_RenderDrawRect(renderer, &thumbRect);
+    }
 }
 
 /*
