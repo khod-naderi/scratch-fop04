2d37c2a7bb446676bcce49e93282db1457eaecdd
khod-naderi <khod.naderi@gmail.com>
2026-02-12 20:43:59 +0330

    remove that "foshe bad" code from workspace.cpp using Ai because it's super confusing !!!

--- include/workspace.h
+++ include/workspace.h
@@ -14,20 +14,10 @@ This header file is for managing code view named workspace
 #ifndef WORKSPACE_HEADERS
 #define WORKSPACE_HEADERS
 
-// Legacy CodeBlock struct
-struct CodeBlock
-{
-    int id;            // identifier
-    int posX, posY;    // position related to workspace cordinate
-    int blockMaster;   // index of block defined in blocksLibrary
-    int topId = -1;    // top block id, -1 if not connected
-    int bottomId = -1; // bottom block id, -1 if not connected
-};
-
 #define CONNECTION_MINIMUM_DISTANCE 15
 
+// All on code block will live here
 extern CodeSpace workspaceCodeSpace;
-extern std::vector<CodeBlock> activeCodeBlocks; // Legacy
 
 void controlWorkspaceClickUp(const int mouseX, const int mouseY);
 void controlWorkspaceClickDown(const int mouseX, const int mouseY);
--- src/workspace.cpp
+++ src/workspace.cpp
@@ -16,17 +16,12 @@ This CPP file is for managing code view named workspace
 #include <cmath>
 #include <iostream>
 
-// Legacy system
-std::vector<CodeBlock> activeCodeBlocks;
-int lastId = 0;
-
-// New system
 CodeSpace workspaceCodeSpace;
 
 // moving items properties
 bool isMovingItem = false;
-int movingTopSelectedItemIndex;
-std::vector<int> movingStringIndesis;
+int movingInstanceId = -1;
+std::vector<int> movingChainIds;
 
 // Scroll workspace properties
 bool isScrollingWorkspace = false;
@@ -36,19 +31,17 @@ int scrollOffsetY = 0;
 
 /*
 -------------------------------
-This function check if any other item that does not have bottom connection is near the top this item.
+This function check if any BlockInstance that can have bottom connection is near the top of this item.
 -------------------------------
 @input:
-TL: top left
 TL_X: x position of top left corner of this item (related to workspace cordinate).
 TL_Y: y position of top left corner of this item (related to workspace cordinate).
 @output:
-if yes, it's will return Id of that item,
-if not, it's will return -1
+if yes, it will return instanceId of that item,
+if not, it will return -1
 */
 int isItemNearTopToConnect(const int TL_X, const int TL_Y)
 {
-    // Check in new CodeSpace system
     for (int i = 0; i < MAX_INSTANCES; i++)
     {
         if (!workspaceCodeSpace.instanceUsed[i])
@@ -69,37 +62,6 @@ int isItemNearTopToConnect(const int TL_X, const int TL_Y)
         }
     }
 
-    // Fallback to legacy system for compatibility
-    for (const CodeBlock &item : activeCodeBlocks)
-    {
-        if (item.bottomId != -1 || !blocksLibrary[item.blockMaster].canHaveBottomConnection)
-            continue;
-
-        const int blc_x = item.posX;
-        const int blc_y = item.posY + blocksLibrary[item.blockMaster].baseHeight;
-        if (std::sqrt(std::pow((blc_x - TL_X), 2) + std::pow((blc_y - TL_Y), 2)) <= CONNECTION_MINIMUM_DISTANCE)
-        {
-            return item.id;
-        }
-    }
-
-    return -1;
-}
-
-/*
------------------------
-This function will search for item with specific id in legacy system
------------------------
-return -1 if not found
-*/
-int foundItemIndexById(int id)
-{
-    for (int i = 0; i < activeCodeBlocks.size(); i++)
-    {
-        if (activeCodeBlocks[i].id == id)
-            return i;
-    }
-
     return -1;
 }
 
@@ -112,6 +74,48 @@ return pointer to BlockInstance or NULL if not found
 BlockInstance *findBlockInstanceById(int id)
 {
     return codespaceGetInstance(workspaceCodeSpace, id);
+}
+
+/*
+-----------------------
+Build a chain of connected blocks starting from the given block
+-----------------------
+*/
+void buildMovingChain(int startId, std::vector<int> &chain)
+{
+    chain.clear();
+    int currentId = startId;
+
+    while (currentId != -1)
+    {
+        chain.push_back(currentId);
+        BlockInstance *inst = findBlockInstanceById(currentId);
+        if (!inst)
+            break;
+        currentId = inst->nextId;
+    }
+}
+
+/*
+-----------------------
+Update positions of all blocks in a chain
+-----------------------
+*/
+void updateChainPositions(const std::vector<int> &chain, int startX, int startY)
+{
+    int currentY = startY;
+
+    for (int id : chain)
+    {
+        BlockInstance *inst = findBlockInstanceById(id);
+        if (!inst)
+            continue;
+
+        Block def = blocksLibrary[inst->defenitionId];
+        inst->posX = startX;
+        inst->posY = currentY;
+        currentY += def.baseHeight;
+    }
 }
 
 /*
@@ -159,55 +163,33 @@ void controlWorkspaceClickUp(const int mouseX, const int mouseY)
                 }
             }
         }
-
-        // Legacy system - create old CodeBlock for compatibility
-        CodeBlock newItem;
-        newItem.posX = posX;
-        newItem.posY = posY;
-        newItem.blockMaster = dragedBlockIndex;
-        newItem.id = lastId++;
-        newItem.bottomId = -1;
-        newItem.topId = -1;
-        activeCodeBlocks.push_back(newItem);
     }
 
-    // fix moving string - handle both new and legacy systems
-    if (isMovingItem)
+    // Fix moving chain
+    if (isMovingItem && movingInstanceId != -1)
     {
-        // Handle legacy CodeBlock system
-        if (movingTopSelectedItemIndex != -1 && movingTopSelectedItemIndex < activeCodeBlocks.size())
+        BlockInstance *movingInst = findBlockInstanceById(movingInstanceId);
+        if (movingInst && blocksLibrary[movingInst->defenitionId].canHaveTopConnection)
         {
-            if (blocksLibrary[activeCodeBlocks[movingTopSelectedItemIndex].blockMaster].canHaveTopConnection)
+            int topId = isItemNearTopToConnect(movingInst->posX, movingInst->posY);
+            if (topId != -1)
             {
-                activeCodeBlocks[movingTopSelectedItemIndex].topId = isItemNearTopToConnect(activeCodeBlocks[movingTopSelectedItemIndex].posX, activeCodeBlocks[movingTopSelectedItemIndex].posY);
-                if (activeCodeBlocks[movingTopSelectedItemIndex].topId != -1)
+                BlockInstance *topInst = findBlockInstanceById(topId);
+                if (topInst)
                 {
-                    int topItemIndex = foundItemIndexById(activeCodeBlocks[movingTopSelectedItemIndex].topId);
-                    if (topItemIndex != -1)
-                    {
-                        activeCodeBlocks[topItemIndex].bottomId = activeCodeBlocks[movingTopSelectedItemIndex].id;
+                    Block topDef = blocksLibrary[topInst->defenitionId];
+                    int newX = topInst->posX;
+                    int newY = topInst->posY + topDef.baseHeight;
 
-                        int totalHeight = 0;
-                        for (int i : movingStringIndesis)
-                        {
-                            if (i < activeCodeBlocks.size())
-                            {
-                                activeCodeBlocks[i].posX = activeCodeBlocks[topItemIndex].posX;
-                                activeCodeBlocks[i].posY = activeCodeBlocks[topItemIndex].posY + blocksLibrary[activeCodeBlocks[topItemIndex].blockMaster].baseHeight + totalHeight;
-                                totalHeight += blocksLibrary[activeCodeBlocks[i].blockMaster].baseHeight;
-                            }
-                        }
-                    }
+                    updateChainPositions(movingChainIds, newX, newY);
+                    codespaceAttachNext(workspaceCodeSpace, topId, movingInstanceId);
                 }
             }
         }
 
-        // TODO: Handle BlockInstance system moving and reconnection
-        // For now, BlockInstances are detached when moved and need manual reconnection
-
         isMovingItem = false;
-        movingStringIndesis.clear();
-        movingTopSelectedItemIndex = -1;
+        movingChainIds.clear();
+        movingInstanceId = -1;
     }
 
     if (isScrollingWorkspace) // Disable Scrolling
@@ -233,7 +215,7 @@ void controlWorkspaceClickDown(const int mouseX, const int mouseY)
 
     bool isClickedOnItem = false;
 
-    // Check BlockInstances first (new system)
+    // Check BlockInstances
     for (int i = 0; i < MAX_INSTANCES && !isClickedOnItem; i++)
     {
         if (!workspaceCodeSpace.instanceUsed[i])
@@ -253,53 +235,15 @@ void controlWorkspaceClickDown(const int mouseX, const int mouseY)
         {
             isMovingItem = true;
             isClickedOnItem = true;
+            movingInstanceId = inst.instanceId;
 
             // Detach from parent connections
             codespaceDetach(workspaceCodeSpace, inst.instanceId);
 
-            // TODO: Implement full chain moving for BlockInstance system
+            // Build the chain of connected blocks
+            buildMovingChain(movingInstanceId, movingChainIds);
 
             break;
-        }
-    }
-
-    // Check legacy CodeBlocks if no BlockInstance was clicked
-    if (!isClickedOnItem)
-    {
-        for (CodeBlock &item : activeCodeBlocks)
-        {
-            SDL_Rect itemRect = {
-                WORKSPACE_COLUMN.x + item.posX + scrollOffsetX,
-                WORKSPACE_COLUMN.y + item.posY + scrollOffsetY,
-                blocksLibrary[item.blockMaster].baseWidth,
-                blocksLibrary[item.blockMaster].baseHeight,
-            };
-
-            if (isPointInRect(mouseX, mouseY, itemRect)) // found grabbed item
-            {
-                isMovingItem = true;
-                isClickedOnItem = true;
-                movingTopSelectedItemIndex = foundItemIndexById(item.id);
-
-                // disconnect from it's top
-                int itemTopIndex = foundItemIndexById(item.topId);
-                if (itemTopIndex != -1)
-                    activeCodeBlocks[itemTopIndex].bottomId = -1;
-                item.topId = -1;
-
-                // found moving string
-                movingStringIndesis.clear();
-                movingStringIndesis.push_back(movingTopSelectedItemIndex);
-                for (int i = movingTopSelectedItemIndex;;)
-                {
-                    i = foundItemIndexById(activeCodeBlocks[i].bottomId);
-                    if (i == -1)
-                        break;
-                    movingStringIndesis.push_back(i);
-                }
-
-                break;
-            }
         }
     }
 
@@ -316,7 +260,6 @@ void controlWorkspaceClickDown(const int mouseX, const int mouseY)
 --------------------------------------------
 This function is for controling mouse motion on WorkSpace
 --------------------------------------------
-
 - move items
 */
 void controlWorkspaceMouseMotion(const int mouseX, const int mouseY)
@@ -325,45 +268,21 @@ void controlWorkspaceMouseMotion(const int mouseX, const int mouseY)
     if (!isPointInRect(mouseX, mouseY, WORKSPACE_COLUMN))
         return;
 
-    // update moving string - handle both systems
-    if (isMovingItem)
+    // update moving chain
+    if (isMovingItem && movingInstanceId != -1)
     {
         // relative position to workspace coordinate
         const int relX = mouseX - WORKSPACE_COLUMN.x - scrollOffsetX;
         const int relY = mouseY - WORKSPACE_COLUMN.y - scrollOffsetY;
 
-        // Handle legacy CodeBlock system
-        if (!movingStringIndesis.empty())
+        BlockInstance *movingInst = findBlockInstanceById(movingInstanceId);
+        if (movingInst)
         {
-            int totalHeight = 0;
-            for (int i : movingStringIndesis)
-            {
-                if (i < activeCodeBlocks.size())
-                {
-                    activeCodeBlocks[i].posX = relX - blocksLibrary[dragedBlockIndex].baseWidth / 2;
-                    activeCodeBlocks[i].posY = relY - blocksLibrary[dragedBlockIndex].baseHeight / 2 + totalHeight;
-                    totalHeight += blocksLibrary[activeCodeBlocks[i].blockMaster].baseHeight;
-                }
-            }
-        }
+            Block def = blocksLibrary[movingInst->defenitionId];
+            int newX = relX - def.baseWidth / 2;
+            int newY = relY - def.baseHeight / 2;
 
-        // Handle BlockInstance system - update position of moved blocks
-        for (int i = 0; i < MAX_INSTANCES; i++)
-        {
-            if (!workspaceCodeSpace.instanceUsed[i])
-                continue;
-
-            BlockInstance *inst = &workspaceCodeSpace.instances[i];
-
-            // Check if this instance is being moved (has no parent)
-            if (inst->parentId == -1)
-            {
-                Block def = blocksLibrary[inst->defenitionId];
-                // Update position for root blocks that might be moving
-                inst->posX = relX - def.baseWidth / 2;
-                inst->posY = relY - def.baseHeight / 2;
-                break; // Only move one block for now (I do not know what should do next, It is so confusing !!!!)
-            }
+            updateChainPositions(movingChainIds, newX, newY);
         }
     }
 
@@ -378,7 +297,6 @@ void controlWorkspaceMouseMotion(const int mouseX, const int mouseY)
 
 void drawWorkspaceScreen(SDL_Renderer *renderer, TTF_Font *font, const int mouseX, const int mouseY)
 {
-
     // Draw shadow for connecting draged item
     if (isBLockDraged)
     {
@@ -387,46 +305,21 @@ void drawWorkspaceScreen(SDL_Renderer *renderer, TTF_Font *font, const int mouse
 
         if (blocksLibrary[dragedBlockIndex].canHaveTopConnection)
         {
-            int nearItemIndex = foundItemIndexById(isItemNearTopToConnect(TL_X, TL_Y));
-            if (nearItemIndex != -1) // there is a near connectable item
+            int nearItemId = isItemNearTopToConnect(TL_X, TL_Y);
+            if (nearItemId != -1) // there is a near connectable item
             {
-                // Draw draged item centre on mouse pointer
-                SDL_Rect dragedItemRect = {
-                    WORKSPACE_COLUMN.x + activeCodeBlocks[nearItemIndex].posX + scrollOffsetX,
-                    WORKSPACE_COLUMN.y + activeCodeBlocks[nearItemIndex].posY + blocksLibrary[activeCodeBlocks[nearItemIndex].blockMaster].baseHeight + scrollOffsetY,
-                    blocksLibrary[dragedBlockIndex].baseWidth,
-                    blocksLibrary[dragedBlockIndex].baseHeight,
-                };
-
-                // Fill item background
-                SDL_SetRenderDrawColor(renderer, color_softGray);
-                SDL_RenderFillRect(renderer, &dragedItemRect);
-            }
-        }
-    }
-
-    // Draw shadow for connecting moving string
-    if (isMovingItem)
-    {
-        if (blocksLibrary[activeCodeBlocks[movingTopSelectedItemIndex].blockMaster].canHaveTopConnection)
-        {
-            int topItemId = isItemNearTopToConnect(activeCodeBlocks[movingTopSelectedItemIndex].posX, activeCodeBlocks[movingTopSelectedItemIndex].posY);
-            if (topItemId != -1) //  there is a near connectable item
-            {
-                int topItemIndex = foundItemIndexById(topItemId);
-
-                int totalHeight = 0;
-                for (int i : movingStringIndesis)
+                BlockInstance *nearInst = findBlockInstanceById(nearItemId);
+                if (nearInst)
                 {
-                    // Draw draged item centre on mouse pointer
-                    SDL_Rect dragedItemRect = {
-                        WORKSPACE_COLUMN.x + activeCodeBlocks[topItemIndex].posX + scrollOffsetX,
-                        WORKSPACE_COLUMN.y + activeCodeBlocks[topItemIndex].posY + blocksLibrary[activeCodeBlocks[topItemIndex].blockMaster].baseHeight + totalHeight + scrollOffsetY,
-                        blocksLibrary[activeCodeBlocks[i].blockMaster].baseWidth,
-                        blocksLibrary[activeCodeBlocks[i].blockMaster].baseHeight,
-                    };
+                    Block nearDef = blocksLibrary[nearInst->defenitionId];
 
-                    totalHeight += blocksLibrary[activeCodeBlocks[i].blockMaster].baseHeight;
+                    // Draw dragged item shadow
+                    SDL_Rect dragedItemRect = {
+                        WORKSPACE_COLUMN.x + nearInst->posX + scrollOffsetX,
+                        WORKSPACE_COLUMN.y + nearInst->posY + nearDef.baseHeight + scrollOffsetY,
+                        blocksLibrary[dragedBlockIndex].baseWidth,
+                        blocksLibrary[dragedBlockIndex].baseHeight,
+                    };
 
                     // Fill item background
                     SDL_SetRenderDrawColor(renderer, color_softGray);
@@ -436,7 +329,49 @@ void drawWorkspaceScreen(SDL_Renderer *renderer, TTF_Font *font, const int mouse
         }
     }
 
-    // Draw BlockInstances from new system
+    // Draw shadow for connecting moving chain
+    if (isMovingItem && movingInstanceId != -1)
+    {
+        BlockInstance *movingInst = findBlockInstanceById(movingInstanceId);
+        if (movingInst && blocksLibrary[movingInst->defenitionId].canHaveTopConnection)
+        {
+            int topItemId = isItemNearTopToConnect(movingInst->posX, movingInst->posY);
+            if (topItemId != -1) //  there is a near connectable item
+            {
+                BlockInstance *topInst = findBlockInstanceById(topItemId);
+                if (topInst)
+                {
+                    Block topDef = blocksLibrary[topInst->defenitionId];
+                    int totalHeight = 0;
+
+                    for (int id : movingChainIds)
+                    {
+                        BlockInstance *chainInst = findBlockInstanceById(id);
+                        if (!chainInst)
+                            continue;
+
+                        Block chainDef = blocksLibrary[chainInst->defenitionId];
+
+                        // Draw shadow for each block in chain
+                        SDL_Rect shadowRect = {
+                            WORKSPACE_COLUMN.x + topInst->posX + scrollOffsetX,
+                            WORKSPACE_COLUMN.y + topInst->posY + topDef.baseHeight + totalHeight + scrollOffsetY,
+                            chainDef.baseWidth,
+                            chainDef.baseHeight,
+                        };
+
+                        totalHeight += chainDef.baseHeight;
+
+                        // Fill item background
+                        SDL_SetRenderDrawColor(renderer, color_softGray);
+                        SDL_RenderFillRect(renderer, &shadowRect);
+                    }
+                }
+            }
+        }
+    }
+
+    // Draw BlockInstances
     for (int i = 0; i < MAX_INSTANCES; i++)
     {
         if (!workspaceCodeSpace.instanceUsed[i])
@@ -480,53 +415,17 @@ void drawWorkspaceScreen(SDL_Renderer *renderer, TTF_Font *font, const int mouse
         SDL_RenderCopy(renderer, texture, NULL, &textRect);
     }
 
-    // Draw legacy CodeBlocks for compatibility
-    for (const CodeBlock &item : activeCodeBlocks)
-    {
-        SDL_Rect itemRect = {
-            WORKSPACE_COLUMN.x + item.posX + scrollOffsetX,
-            WORKSPACE_COLUMN.y + item.posY + scrollOffsetY,
-            blocksLibrary[item.blockMaster].baseWidth,
-            blocksLibrary[item.blockMaster].baseHeight,
-        };
-
-        // item background
-        if (!isBLockDraged && isPointInRect(mouseX, mouseY, itemRect))
-        {
-            SDL_SetRenderDrawColor(renderer, colorDim(categories[blocksLibrary[item.blockMaster].categoryId].color));
-        }
-        else
-        {
-            SDL_SetRenderDrawColor(renderer, categories[blocksLibrary[item.blockMaster].categoryId].color);
-        }
-        SDL_RenderFillRect(renderer, &itemRect);
-
-        // item border
-        SDL_SetRenderDrawColor(renderer, color_black);
-        SDL_RenderDrawRect(renderer, &itemRect);
-
-        // item text
-        int tw, th;
-        SDL_Texture *texture = renderText(renderer, font, blocksLibrary[item.blockMaster].label, color_white);
-        SDL_QueryTexture(texture, NULL, NULL, &tw, &th);
-        SDL_Rect textRect = {
-            itemRect.x + (itemRect.w - tw) / 2,
-            itemRect.y + (itemRect.h - th) / 2,
-            tw,
-            th,
-        };
-        SDL_RenderCopy(renderer, texture, NULL, &textRect);
-    }
-
     return;
 }
 
 int workspaceScreenInit(SDL_Renderer *renderer, TTF_Font *font)
 {
-    // Initialize the new CodeSpace system
+    // Initialize the CodeSpace system
     codespaceInit(workspaceCodeSpace);
     return 0;
 }
+
+// Helper functions for CodeSpace integration
 
 int createBlockInstanceInWorkspace(int definitionId, int posX, int posY)
 {
